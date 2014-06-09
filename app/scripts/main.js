f31eb2ebcc78cc4b30b6be7504768f27e764de4e
5creations <ross@demogorgon.gateway.2wire.net>
2014-06-09 11:08:27 -0700

    this is 5creations

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+
+console.log('\'Allo \'Allo!');
+
+
+define([], function() {
+    //Start writing your code here
+
+    $("#test").click(function(){
+    	//$("#test").fadeOut();
+    	alert("test");
+    })
+
+});
